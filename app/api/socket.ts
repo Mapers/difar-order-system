522123f54d118c074077dc371e25402d0aed156c
CarlosJhulyan <cmedinallantirhuay@gmail.com>
2025-09-24 15:19:46 -0500

    Fix config socket

--- app/api/socket.ts
+++ app/api/socket.ts
@@ -2,8 +2,6 @@ import {io} from "socket.io-client";
 
 const SOCKET_URL = "https://backend-difar.backend-l23.click/socket.io";
 
-const socket = io(SOCKET_URL, {
-    transports: ['websocket', 'polling']
-});
+const socket = io(SOCKET_URL);
 
 export default socket;
